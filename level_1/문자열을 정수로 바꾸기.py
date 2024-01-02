752079fde3f93e1a759415788d77c3bddf9bb4e2
so1omon <solmin3665@gmail.com>
2024-01-02 12:01:20 +0900

    2024-01-02

---
+++ level_1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,11 @@
+'''
+2024-01-02
+https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+- int 함수는 부호 식별
+'''
+
+def solution(s):
+    return int(s)
+
+print(solution("-1234") + solution("1233"))
---
+++ summary.py
@@ -0,0 +1,33 @@
+# sum([iterable]) : 모든 원소 합 리턴
+
+# lower([str]) / upper([str]) : 대소문자 변환
+
+# [strA].count([strB]) : strA 문자열 내에 strB가 
+
+# int([Object]) 함수는 부호까지 식별
+
+# map([func], [iterable])
+
+# math.sqrt 대신 [number] ** (1/2) 사용 가능
+
+# a부터 b까지의 합 공식 : (a+b)(b-a+1)/2 -> 첫원소, 끝원소 합 * a부터 b까지 길이 / 2
+
+# 음수 정수를 나타내고 싶을 때 (-1)*a 대신에 -a로 표현해도 됨
+
+# enumerate([str]) : 지정한 리스트를 (인덱스 번호, 원소)로 이루어진 튜플 리스트로 변환\
+    
+''' 
+for entry in enumerate(['A', 'B', 'C']):
+    print(entry)
+    
+result)
+(0, 'A')
+(1, 'B')
+(2, 'C')
+'''
+
+# [list].index([element]) : list 내에 element가 위치한 인덱스 리턴, 없으면 -1
+
+# list(filter(function, iterable)) -> iterable : 조건에 맞는 원소만 걸러서 반환
+
+# int 함수로 n빈법 변환하기 : https://school.programmers.co.kr/learn/courses/30/lessons/68935/solution_groups?language=python3
